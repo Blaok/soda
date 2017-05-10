7efffd395d7fc85c2d1750712a0093cddd3269c4
Blaok <i@blaok.me>
2017-05-10 15:55:16 -0700

    feat: generator emits working code now

--- generate-reuse-buffer.py
+++ generate-reuse-buffer.py
@@ -4,10 +4,10 @@ import math
 import operator
 
 # input
-St = [256,256]
-A = [[-1,-1],[0,-1],[1,-1],[-1,0],[0,0],[1,0],[-1,1],[0,1],[1,1]]
+St = [300,304]
+A = [[0,0],[1,0],[2,0],[0,1],[1,1],[2,1],[0,2],[1,2],[2,2]]
 #A = [[0,-1],[-1,0],[0,0],[1,0],[0,1]]
-k = 16
+k = 3
 
 # serialize
 def Serialize(vec, St):
@@ -29,45 +29,20 @@ def GetChains(St, A, k):
         chains += [[x for x in A_dag if x%k == i]]
     return chains
 
-def PrintChains(chains):
-    k = len(chains)
-    length_chain = int(math.log(k-1, 10)+1)
-    length_buffer = int(math.log(max([x[-1] for x in chains])-1, 10)+1)
-    if(min([x[0] for x in chains]) < 0):
-        length_buffer_neg = int(math.log(-min([x[0] for x in chains])-1, 10)+1)
-        if length_buffer < length_buffer_neg+1:
-            length_buffer = length_buffer_neg+1
-    for i in range(0, k):
-        for j in range(0,len(chains[i])-1):
-            interval_length = chains[i][j+1] - chains[i][j]
-            if interval_length == k:
-                print(("Chain %%%dd: FF   between %%%dd and %%%dd" % (length_chain, length_buffer, length_buffer)) % (i, chains[i][j], chains[i][j+1]))
-            else:
-                print(("Chain %%%dd: FIFO between %%%dd and %%%dd" % (length_chain, length_buffer, length_buffer)) % (i, chains[i][j], chains[i][j+1]))
-
-chains = GetChains(St, A, k)
-#PrintChains(chains)
-
 def GetPoints(A, k):
     all_points = {} # {offset:{unroll_index:point_index}}
     for i in range(0, k):
         points = [Serialize(x, St) for x in A]
         for idx, j in enumerate(points):
-            #            chain_idx = (j+i)%k
-#            fifo_idx = chains[chain_idx].index(j+i)
             all_points[j+i] = all_points.get(j+i, {})
             all_points[j+i][i] = idx
-#            print("Access %d @ Chain %d, FIFO %d" % (j+i, chain_idx,fifo_idx))
-#    print(all_points)
     return all_points
 
-
-def GetBuffer(chains, A):
-    k = len(chains)
+def GetBuffer(St, A, k):
     FFs = []    # [outputs]
     FIFOs = {}  # {length:[outputs]}
     inputs = []
-    for chain in chains:
+    for chain in GetChains(St, A, k):
         inputs += [chain[-1]]
         for j in range(0, len(chain)-1):
             interval_length = chain[j+1]-chain[j]
@@ -81,58 +56,135 @@ def GetBuffer(chains, A):
         FIFO.sort()
     return {'FFs':FFs, 'FIFOs':FIFOs, 'k':k, 'inputs':inputs}
 
-def MapBuffer(buf):
-    mapping = {}    # maps an offset to what consumes it
+def GetConsumeMap(buf):
+    mapping = {'k':buf['k']}    # maps an offset to what consumes it
     for idx, item in enumerate(buf['FFs']):
-        mapping[item+k] = {'FFs':idx}
+        mapping[item+k] = {'FFs':{'index':idx, 'produce':item}}
     for fifo_length in buf['FIFOs'].keys():
         for idx, item in enumerate(buf['FIFOs'][fifo_length]):
-            mapping[item+fifo_length] = {'FIFOs':[fifo_length, idx]}
+            mapping[item+fifo_length] = {'FIFOs':{'length':fifo_length, 'index':idx, 'produce':item}}
     return mapping
 
-def Map2String(mapping):
-    if 'FIFOs' in mapping:
-        return 'FIFO_%d[%d]' % (mapping['FIFOs'][0], mapping['FIFOs'][1])
-    if 'FFs' in mapping:
-        return 'FF[%d]' % (mapping['FFs'])
-
-def PrintBuffer(chains, A):
-    k = len(chains)
-    buf = GetBuffer(chains, A)
-    points = GetPoints(A, k)
-    mapping = MapBuffer(buf)
-#    print(buf)
-#    print(points)
-#    print(mapping)
-    print('input[%d]' % len(buf['inputs']))
-    print('FF[%d]' % len(buf['FFs']))
+def GetProduceMap(buf):
+    mapping = {'k':buf['k']}    # maps an offset to what produces it
+    for idx, item in enumerate(buf['inputs']):
+        mapping[item] = {'inputs':{'index':idx, 'produce':item}}
+    for idx, item in enumerate(buf['FFs']):
+        mapping[item] = {'FFs':{'index':idx, 'produce':item}}
     for fifo_length in buf['FIFOs'].keys():
-        print('FIFO_%d[%d]' % (fifo_length,len(buf['FIFOs'][fifo_length])))
+        for idx, item in enumerate(buf['FIFOs'][fifo_length]):
+            mapping[item] = {'FIFOs':{'length':fifo_length, 'index':idx, 'produce':item}}
+    return mapping
+
+def PrintLine(line = ''):
+    global indent
+    if line:
+        print('%s%s' % (' '*indent*4,line))
+    else:
+        print()
+
+def PrintUpdate(producer_map, consumer_map, src):
+    if 'FIFOs' in src:
+        dst = consumer_map.get(src['FIFOs']['produce'])
+        if not dst:
+            return
+        fifo_length = src['FIFOs']['length']/producer_map['k']
+        fifo_index = src['FIFOs']['index']
+        src_str = 'FIFO_%d[%d][FIFO_%d_p[%d]]' % (fifo_length, fifo_index, fifo_length, fifo_index)
+    elif 'FFs' in src:
+        dst = consumer_map.get(src['FFs']['produce'])
+        if not dst:
+            return
+        src_str = 'FF[%d]' % (src['FFs']['index'])
+    else:
+        dst = consumer_map.get(src['inputs']['produce'])
+        if not dst:
+            return
+        src_str = 'input_buffer[%d]' % (src['inputs']['index'])
+
+    PrintUpdate(producer_map, consumer_map, dst)
+
+    if 'FIFOs' in dst:
+        fifo_length = dst['FIFOs']['length']/consumer_map['k']
+        fifo_index = dst['FIFOs']['index']
+        dst_str = 'FIFO_%d[%d][FIFO_%d_p[%d]]' % (fifo_length, fifo_index, fifo_length, fifo_index)
+    elif 'FFs' in dst:
+        dst_str = 'FF[%d]' % (dst['FFs']['index'])
+    else:
+        return
+
+    PrintLine('%s = %s;' % (dst_str, src_str))
+
+def PrintBuffer(St, A, k):
+    global indent
+    buf = GetBuffer(St, A, k)
+    points = GetPoints(A, k)
+    indent_save = indent
+    indent = 0
+    PrintLine('#if %d!=(UNROLL_FACTOR)' % k)
+    PrintLine('#error UNROLL_FACTOR != %d' % k)
+    PrintLine('#endif')
+    PrintLine()
+    for dim, size in enumerate(St):
+        PrintLine('#if %d!=(TILE_SIZE_DIM%d)' % (size, dim))
+        PrintLine('#error TILE_SIZE_DIM%d != %d' % (dim, size))
+        PrintLine('#endif')
+        PrintLine()
+    PrintLine('#if ((%s*(PIXEL_WIDTH))%%(BURST_WIDTH))!=0' % '*'.join(['(TILE_SIZE_DIM%d)'%x for x in range(0, len(St))]))
+    PrintLine('#error TILE_SIZE%BURST_WIDTH != 0')
+    PrintLine('#endif')
+    PrintLine()
+    indent = indent_save
+
+    PrintLine('input_type input_points[(UNROLL_FACTOR)][%d];' % len(A))
+    for idx, item in enumerate(A):
+        PrintLine('//         input_points[(UNROLL_FACTOR)][%d] <=> (%s)' % (idx, str(item)[1:-1]))
+    PrintLine()
+    PrintLine('input_type input_buffer[%d];' % len(buf['inputs']))
+    PrintLine('input_type FF[%d];' % len(buf['FFs']))
+    PrintLine()
+    for fifo_length in buf['FIFOs'].keys():
+        PrintLine('input_type FIFO_%d[%d][%d];' % (fifo_length/k, len(buf['FIFOs'][fifo_length]), fifo_length/k))
+        PrintLine('uint32_t FIFO_%d_p[%d] = {%s};' % (fifo_length/k, len(buf['FIFOs'][fifo_length]), str([0]*len(buf['FIFOs'][fifo_length]))[1:-1]))
+        PrintLine()
+
+    PrintLine('for(uint32_t unroll_index = 0; unroll_index<(UNROLL_FACTOR); ++unroll_index)')
+    PrintLine('{')
+    indent_save = indent
+    indent = 0
+    PrintLine('#pragma HLS unroll')
+    indent = indent_save+1
+    PrintLine('if(input_index*(UNROLL_FACTOR)+unroll_index < %s)' % ('*'.join(['(TILE_SIZE_DIM%d)'%x for x in range(0, len(St))])))
+    PrintLine('{')
+    indent = indent_save+2
+    PrintLine('input_buffer[unroll_index] = input[input_index*(UNROLL_FACTOR)+unroll_index];')
+    indent = indent_save+1
+    PrintLine('}')
+    indent = indent_save
+    PrintLine('}')
+    PrintLine()
+
     for idx, item in enumerate(buf['inputs']):
         for unroll_index in points[item].keys():
-            print("access %4d from input[%d]     when unroll_index = %d for stencil point %s" % (item, idx, unroll_index, str(A[points[item][unroll_index]])))
+            PrintLine("input_points[%d][%d] = input_buffer[%d]; // (%s)" % (unroll_index, points[item][unroll_index], idx, str(A[points[item][unroll_index]])[1:-1]))
     for idx, item in enumerate(buf['FFs']):
         for unroll_index in points[item].keys():
-            print("access %4d from FF[%d]        when unroll_index = %d for stencil point %s" % (item, idx, unroll_index, str(A[points[item][unroll_index]])))
+            PrintLine("input_points[%d][%d] = FF[%d]; // (%s)" % (unroll_index, points[item][unroll_index], idx, str(A[points[item][unroll_index]])[1:-1]))
     for fifo_length in buf['FIFOs'].keys():
         for idx, item in enumerate(buf['FIFOs'][fifo_length]):
             for unroll_index in points[item].keys():
-                print("access %4d from FIFO_%d[%d]  when unroll_index = %d for stencil point %s" % (item, fifo_length, idx, unroll_index, str(A[points[item][unroll_index]])))
-    for idx, item in enumerate(buf['inputs']):
-        if item in mapping:
-            print('feed %s to %s' % ('inputs[%d]'%idx, Map2String(mapping[item])))
+                PrintLine("input_points[%d][%d] = FIFO_%d[%d][FIFO_%d_p[%d]]; // (%s)" % (unroll_index, points[item][unroll_index], fifo_length/k, idx, fifo_length/k, idx, str(A[points[item][unroll_index]])[1:-1]))
+    PrintLine()
 
-    for idx, item in enumerate(buf['FFs']):
-        if item in mapping:
-            print('feed %s to %s' % ('FF[%d]'%idx, Map2String(mapping[item])))
-        else:
-            print('pop FF[%d]' % idx)
+    for idx, item in enumerate(buf['inputs']):
+        PrintUpdate(GetProduceMap(buf), GetConsumeMap(buf), {'inputs':{'index':idx, 'produce':item}})
+        PrintLine()
+
     for fifo_length in buf['FIFOs'].keys():
         for idx, item in enumerate(buf['FIFOs'][fifo_length]):
-            if item in mapping:
-                print('feed %s to %s' % ('FIFO_%d[%d]'%(fifo_length, idx), Map2String(mapping[item])))
-            else:
-                print('pop FIFO_%d[%d]' % (fifo_length, idx))
+            fifo = 'FIFO_%d_p[%d]' % (fifo_length/k, idx)
+            PrintLine('%s = %s==%d-1 ? 0 : %s+1;' % (fifo, fifo, fifo_length/k, fifo))
 
-PrintBuffer(chains, A)
+indent = 4
+PrintBuffer(St, A, k)
 
