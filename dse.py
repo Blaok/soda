3b633e83658dbef621a0d2619adcc566c4304ab6
Blaok <i@blaok.me>
2017-09-26 13:35:35 -0700

    chore: change model

--- dse.py
+++ dse.py
@@ -6,14 +6,18 @@ import sys
 
 # design parameters
 # tile size
-st = [128, 128]
+St = [2000, 0]
 
 # unroll factor
-k = 1
+k = 16
+
+# burst length
+Sb = 100032
 
 # application-specific parameters
 # output image size
-S = [2560, 1920]
+S = [2000, 1000]
+St[-1] = S[-1]
 
 # input data element width in bit
 Wi = [16]
@@ -22,7 +26,7 @@ Wi = [16]
 Wo = [8, 8, 8]
 
 # stencil reuse distance
-Sr = st[0]*10+2
+#Sr = st[0]*10+2
 
 # platform-specific parameters
 # DRAM read latency in cycle (542ns)
@@ -49,120 +53,53 @@ Wb = 512
 # deduced parameters
 # total execution latency
 def te():
-    return (mul(nt())+2)*(max(tl(), ts()))
+    return (mul(nt())*ceil(mul(St)/Sb)+2)*(max(tc(), tl(), ts()))
 
 # tile number
 def nt():
-    return [ceil(Si/(sti-Ssi+1)) for (Si, sti, Ssi) in zip(S, st, Ss)]
+    return [ceil((Si-Ssi+1)/(Sti-Ssi+1)) for (Si, Sti, Ssi) in zip(S, St, Ss)]
 
 # compute latency
 def tc():
-    return ceil((mul(st)+Sr-1)*IIc()/k)
+    return ceil(Sb/k)
 
 # load latency
 def tl():
-    #    return ceil(mul(st)/floor(Wb/sum(Wi)))*Wb/BWr+Lr
-    return ceil(mul(st)/floor(Wb/sum(Wi)))*Wb/BWr*2+Lr
-#return max(ceil(mul(st)/floor(Wb/sum(Wi)))*Wb/(BWr*sum(Wi)/(sum(Wi)+sum(Wo)))+Lr, IIl()*ceil(mul(st)*sum(Wi)/Wb))
+    return ceil(sum(Wi)*Sb/BWr+Lr)*5/3
 
 # store latency
 def ts():
-    #return ceil(mul(st)/floor(Wb/sum(Wo)))*Wb/BWw+Lw
-    return ceil(mul(st)/floor(Wb/sum(Wo)))*Wb/BWw*2+Lw
-#return max(ceil(mul(st)/floor(Wb/sum(Wo)))*Wb/(BWw*sum(Wo)/(sum(Wi)+sum(Wo)))+Lw, IIs()*ceil(mul(st)*sum(Wo)/Wb))
-
-# compute II
-def IIc():
-    return 1
-#    return max(ceil(k/pi), ceil(k/po))
-
-# load II
-def IIl():
-    print("load II: %d" % ceil(floor(Wb/max(Wi))/pi))
-    return ceil(floor(Wb/max(Wi))/pi)
-
-# store II
-def IIs():
-    print("store II: %d" % ceil(floor(Wb/max(Wo))/po))
-    return ceil(floor(Wb/max(Wo))/po)
+    return ceil(sum(Wo)*Sb/BWw+Lw)*5/2
 
 # helper
 def mul(l):
     return functools.reduce(operator.mul, l)
 
-# FPGA BRAM size in bit
-BRAM = 6000000*8*.06
+default_St0 = St[0]
+for St0 in [100, 200, 500, 1000, 1500, 2000]:
+    St[0] = St0
+    print("St0 = %d,\tk = %d,\tSb = %d, \t%.3f pixel/ns" % (St[0], k, Sb, mul(S)/((te())/f*1000)))
+St[0] = default_St0
 
-k = 16
-pi = 16
-po = 32
+default_k = k
+for k in [1, 2, 4, 8, 16, 32]:
+    if k == default_k:
+        continue
+    print("St0 = %d,\tk = %d,\tSb = %d, \t%.3f pixel/ns" % (St[0], k, Sb, mul(S)/((te())/f*1000)))
+k = default_k
 
-#S = [1998, 998]
-#Ss = [5, 5]
-st = [256, 498]
-#Wi = [16]
-#Wo = [16]
-#Lr = 120
-#Lw = 120
-#BWr = 12.8*8*5
-#BWw = 12.8*8*5
+default_Sb = Sb
+for Sb in [4992, 9984, 20032, 49984, 100032, 200000]:
+    if Sb == default_Sb:
+        continue
+    print("St0 = %d,\tk = %d,\tSb = %d, \t%.3f pixel/ns" % (St[0], k, Sb, mul(S)/((te())/f*1000)))
+Sb = default_Sb
 
-print("%d us" % round((te())/200))
-
-stmin = st
-temin = 10000000000000000000000000
-
-pixel_group = int(Wb/max(max(Wi),max(Wo)))
-
-for st0 in range(round(ceil((Ss[0]-1)/(Wb/max(max(Wi),max(Wo))))*Wb/max(max(Wi),max(Wo))/2), round(min(S[0], round(BRAM/2/(sum(Wi)+sum(Wo))/(Ss[1]-1)/10))/2)+1):
-    for st1 in range((Ss[1]-1)*10, min(S[1], round(BRAM/2/(sum(Wi)+sum(Wo))/st0/2))+1):
-        st = [2*st0, 2*st1]
-        newte = te()
-        if newte < temin and mul(st) % po == 0:
-            temin = newte
-            kmin = k
-            stmin = st
-
-print("best we can do")
-print("min te: "+str(temin/f))
-st = stmin
-Lr = 120
-Lw = 120
-BWr = 12.8*8*5
-BWw = 12.8*8*5
-print("min te: "+str(te()/f))
-print("k: "+str(kmin))
-print("st: "+str(stmin))
-print("pi: "+str(pi))
-print("po: "+str(po))
-
-sys.exit(0)
-print("let's try to get within 5% of the best")
-
-for k in reversed(range(kmin, kmin+1)):
-    print(k)
-    for st0 in range((Ss[0]-1)*10, min(S[0], round(BRAM/2/(Wi+Wo)/(Ss[1]-1)/10))+1):
-        for st1 in range((Ss[1]-1)*10, min(S[1], round(BRAM/2/(Wi+Wo)/st0))+1):
-            st = [st0, st1]
-            for pi in reversed(range(pi0(), pimax+1)):
-                cont = True
-                for po in reversed(range(po0(), pomax+1)):
-                    newte = te()
-                    if newte < temin:
-                        temin = newte
-                        kmin = k
-                        stmin = st
-                        pimin = pi
-                        pomin = po
-                    else:
-                        cont = False
-                        break
-                if not cont:
-                    break
-
-print("min te: "+str(temin))
-print(" k: "+str(kmin))
-print(" st: "+str(stmin))
-print(" pi: "+str(pimin))
-print(" po: "+str(pomin))
-
+default_S0 = S[0]
+S[0] = 20000
+default_St0 = St[0]
+for St0 in [100, 200, 500, 1000, 1500, 2000]:
+    St[0] = St0
+    print("St0 = %d,\tk = %d,\tSb = %d, \t%.3f pixel/ns" % (St[0], k, Sb, mul(S)/((te())/f*1000)))
+St[0] = default_St0
+S[0] = default_S0
