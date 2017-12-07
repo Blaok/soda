5785224ef084752784e6e61b03523a33c3789d94
Blaok <i@blaok.me>
2017-12-07 10:01:24 -0800

    feat: connect with kernel generator

--- supo.py
+++ supo.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
+from functools import reduce
 from os.path import join, dirname
-import logging
 from textx import metamodel_from_file
-from textx.export import metamodel_export, model_export
 from textx.exceptions import TextXSyntaxError
+import logging
+import operator
+import sys
+import supo_code_gen
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class SupoProgram(object):
     def __init__(self, **kwargs):
+        self.app_name = kwargs.pop('app_name')
         self.burst_width = kwargs.pop('burst_width')
         self.dram_chan = kwargs.pop('dram_chan')
         self.dram_separate = kwargs.pop('dram_separate') == 'yes'
+        self.k = kwargs.pop('k')
         self.input = kwargs.pop('input')
+        self.tile_size = self.input.tile_size
+        self.dim = len(self.tile_size)
         # TODO: get dims
         self.output = kwargs.pop('output')
         # TODO: check dims
+        self.A = self.output.GetA()
+        print(self.A)
+        self.compute_content = self.output.GetCode().split('\n')
+        self.extra_params = None
 
     def __str__(self):
         return \
             ('burst width: %d\n' % self.burst_width) + \
             ('dram channel: %d\n' % self.dram_chan) + \
             ('dram separate: %s\n' % ('yes' if self.dram_separate else 'no')) + \
-            ('input %s: %s(%s)\n' % (self.input.type, self.input.name, ', '.join([str(x) for x in self.input.tile_size] + ['']))) + \
-            ('output %s: %s(%s) = %s\n' % (self.output.type, self.output.name, ', '.join([str(x) for x in self.output.idx]), self.output.expr)) + \
+            ('%s\n' % self.input) + \
+            ('%s\n' % self.output) + \
             ''
 
 class Expression(object):
@@ -44,6 +55,9 @@ class Expression(object):
                 operand.GetCode()+' '+operator+' ' for operand, operator
                     in zip(self.operand, self.operator)]), self.operand[-1].GetCode())
 
+    def GetLoadIndices(self):
+        return reduce(operator.add, [op.GetLoadIndices() for op in self.operand])
+
 class Term(object):
     def __init__(self, **kwargs):
         self.operand = kwargs.pop('operand')
@@ -61,6 +75,9 @@ class Term(object):
                 operand.GetCode()+' '+operator+' ' for operand, operator
                     in zip(self.operand, self.operator)]), self.operand[-1].GetCode())
 
+    def GetLoadIndices(self):
+        return reduce(operator.add, [op.GetLoadIndices() for op in self.operand])
+
 class Factor(object):
     def __init__(self, **kwargs):
         self.operand = kwargs.pop('operand')
@@ -72,9 +89,15 @@ class Factor(object):
     def GetCode(self):
         return ('-%s' if self.sign=='-' else '%s') % self.operand.GetCode()
 
+    def GetLoadIndices(self):
+        return self.operand.GetLoadIndices()
+
 class Operand(object):
     def __init__(self, **kwargs):
         self.name = kwargs.pop('name')
+        self.chan = kwargs.pop('chan')
+        if not self.chan:
+            self.chan = 1
         self.idx = kwargs.pop('idx')
         self.num = kwargs.pop('num')
         self.expr = kwargs.pop('expr')
@@ -95,17 +118,73 @@ class Operand(object):
         if self.expr:
             return '(%s)' % self.expr.GetCode()
 
+    def GetLoadIndices(self):
+        if self.expr:
+            return self.expr.GetLoadIndices()
+        if self.num:
+            return []
+        if self.name:
+            return [self.idx]
+
+class Input(object):
+    def __init__(self, **kwargs):
+        self.type = kwargs.pop('type')
+        self.name = kwargs.pop('name')
+        self.chan = kwargs.pop('chan')
+        if not self.chan:
+            self.chan = 1
+        self.tile_size = kwargs.pop('tile_size')+[0]
+
+    def __str__(self):
+        return ('input %s: %s(%s)' % (self.type, self.name, ', '.join([str(x) for x in self.tile_size[0:-1]] + [''])))
+
+class Output(object):
+    def __init__(self, **kwargs):
+        self.type = kwargs.pop('type')
+        self.name = kwargs.pop('name')
+        self.chan = kwargs.pop('chan')
+        if not self.chan:
+            self.chan = 1
+        self.idx = kwargs.pop('idx')
+        self.expr = kwargs.pop('expr')
+
+    def __str__(self):
+        return ('output %s: %s(%s) = %s' % (self.type, self.name, ', '.join([str(x) for x in self.idx]), self.expr))
+
+    def GetCode(self):
+        return 'output_type result = '+self.expr.GetCode()
+
+    def GetA(self):
+        return self.expr.GetLoadIndices()
+
 def main():
     this_folder = dirname(__file__)
-    supo_mm = metamodel_from_file(join(this_folder, 'supo.tx'), classes=[SupoProgram, Expression, Term, Factor, Operand])
+    supo_mm = metamodel_from_file(join(this_folder, 'supo.tx'), classes=[SupoProgram, Expression, Term, Factor, Operand, Input, Output])
     logger.info('Built metamodel.')
     try:
         supo_model = supo_mm.model_from_file(join(this_folder, 'test.supo'))
     except TextXSyntaxError as e:
         logger.error(e)
     else:
-        print(supo_model)
-        print(supo_model.output.expr.GetCode())
+        stencil = supo_code_gen.Stencil(
+            burst_width = supo_model.burst_width,
+            dram_chan = supo_model.dram_chan,
+            app_name = supo_model.app_name,
+            input_name = supo_model.input.name,
+            input_type = supo_model.input.type,
+            input_chan = supo_model.input.chan,
+            output_name = supo_model.output.name,
+            output_type = supo_model.output.type,
+            output_chan = supo_model.output.chan,
+            A = supo_model.A,
+            dim = supo_model.dim,
+            compute_content = supo_model.compute_content,
+            extra_params = supo_model.extra_params,
+            tile_size = supo_model.tile_size,
+            k = supo_model.k,
+            dram_separate = supo_model.dram_separate)
+        with open('%s_kernel.cpp' % supo_model.app_name, 'w') as kernel_file:
+            supo_code_gen.PrintCode(stencil, kernel_file)
 
 if __name__ == '__main__':
     main()
