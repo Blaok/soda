09a4548b086def9f4b301fcb00c0093d47cdf990
Blaok <i@blaok.me>
2018-05-17 15:30:33 -0700

    feat: use external BRAM model

--- src/supo/model/xilinx.py
+++ src/supo/model/xilinx.py
@@ -1,16 +1,27 @@
 import json
 import logging
 import math
+import subprocess
 
 from supo.generator.utils import *
 from supo.model.resource import *
 
 logger = logging.getLogger('__main__').getChild(__name__)
 
+record = {}
+
 def accumulate_fifo(estimation_routed, width, depth):
     fifo_size = depth*width
     if fifo_size > 1024:
-        estimation_routed['BRAM'] += math.ceil(fifo_size/32768)
+        inc = 16*1024//width
+        modulo_depth = math.ceil(depth/inc)*inc
+        query_proc = subprocess.Popen([os.environ['XILINX_BRAM_MODEL_PATH'] + '/query', str(width), str(modulo_depth)], stdout=subprocess.PIPE, bufsize=0)
+        delta = json.loads(query_proc.stdout.read())
+        delta = delta['fifo_w%d_d%d' % (width, modulo_depth)]['BRAM_18K'] + delta['fifo_w%d_d%d' % (width, modulo_depth)]['BRAM_36K']*2
+
+        estimation_routed['BRAM'] += delta
+        record.setdefault((width, depth, delta), 0)
+        record[(width, depth, delta)] += 1
     else:
         estimation_routed['REG'] += width
         estimation_routed['LUT'] += depth/2
@@ -47,8 +58,7 @@ def print_estimation(stencil, model_file, output_file):
 
     # axi master
     accumulate_resources(estimation_hls, model['m_axi'],
-                         stencil.input.chan*stencil.dram_bank +
-                         stencil.output.chan*stencil.dram_bank)
+                         stencil.dram_bank*2)
 
     # load/store
     accumulate_resources(estimation_hls, model['load'],
@@ -78,6 +88,7 @@ def print_estimation(stencil, model_file, output_file):
 
     # modules in the dataflow graph
     super_source = stencil.dataflow_super_source
+    repli = stencil.replication_factor if stencil.replication_factor is not None else 1
     for node in super_source.bfs_node_generator():
         # forward modules
         if isinstance(node, ForwardNode):
@@ -86,13 +97,13 @@ def print_estimation(stencil, model_file, output_file):
                 msg = 'forward<%s> not found in model %s' % (node.tensor.type,
                                                              model_file.name)
                 raise SemanticError(msg)
-            accumulate_resources(estimation_hls, model[forward_key]['base'])
+            accumulate_resources(estimation_hls, model[forward_key]['base'], repli)
             accumulate_resources(estimation_hls,
                                  model[forward_key]['overhead_per_dst'],
-                                 len(node.children))
+                                 len(node.children)*repli)
             if node.depth > 0:
                 accumulate_resources(estimation_hls,
-                                     model[forward_key]['overhead_of_depth'])
+                                     model[forward_key]['overhead_of_depth'], repli)
         elif isinstance(node, ComputeNode):
             if node.stage.name in (stencil.output.name,):
                 stage_name = node.stage.name
@@ -104,7 +115,7 @@ def print_estimation(stencil, model_file, output_file):
                 msg = 'unknown dataflow compute node: %s' % node.stage.name
                 raise SemanticError(msg)
             accumulate_resources(estimation_hls,
-                                 model['compute'][stencil.app_name][stage_name])
+                                 model['compute'][stencil.app_name][stage_name], repli)
 
     performance = stencil.unroll_factor*model['target_freq']/10**3  # pixel/ns
     dram_bandwidth = model['dram_bandwidth']
@@ -124,24 +135,27 @@ def print_estimation(stencil, model_file, output_file):
     estimation_routed = estimation['resource_routed']
 
     estimation_routed['DSP'] = estimation_hls['DSP48E']
-    estimation_routed['BRAM'] = estimation_hls['BRAM_18K']/2
-    estimation_routed['REG'] = estimation_hls['FF']*model['ff_coeff']
-    estimation_routed['LUT'] = estimation_hls['LUT']*model['lut_coeff']
+    estimation_routed['BRAM'] = estimation_hls['BRAM_18K']
+    estimation_routed['REG'] = estimation_hls['FF']*model['ff_coeff']+model['ff_base']
+    estimation_routed['LUT'] = estimation_hls['LUT']*model['lut_coeff']+model['lut_base']
     estimation_routed['LUTMem'] = estimation_hls['LUT']*model['lutmem_coeff']
 
     # BRAMs
     for src_node, dst_node in super_source.bfs_edge_generator():
         if isinstance(dst_node, ForwardNode):
-            accumulate_fifo(estimation_routed,
-                            type_width[dst_node.tensor.type],
-                            dst_node.depth)
+            for c in range(dst_node.tensor.chan*repli):
+                accumulate_fifo(estimation_routed,
+                                type_width[dst_node.tensor.type],
+                                max(dst_node.depth, 1))
         elif (isinstance(src_node, ForwardNode) and
               isinstance(dst_node, ComputeNode)):
             extra_depth = super_source.get_extra_depth((src_node, dst_node))
-            accumulate_fifo(estimation_routed,
-                            type_width[src_node.tensor.type],
-                            max(extra_depth+1, 2))
+            for c in range(src_node.tensor.chan*repli):
+                accumulate_fifo(estimation_routed,
+                                type_width[src_node.tensor.type],
+                                max(extra_depth+1, 2))
 
+    estimation_routed['BRAM'] = math.ceil(estimation_routed['BRAM']/2)
 
     available_hls = model['available_hls']
     available_routed = model['available_routed']
