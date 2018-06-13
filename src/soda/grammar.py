c098bf15c8104c311a9b46a4e10ced2319eff1bf
Blaok <i@blaok.me>
2018-06-13 13:33:31 -0700

    fix: use Integer when Float is not appropriate

--- src/soda/grammar.py
+++ src/soda/grammar.py
@@ -31,7 +31,7 @@ Comment: /\s*#.*$/;
 Dec: /\d+([Uu][Ll][Ll]?|[Ll]?[Ll]?[Uu]?)/;
 Expression: operand=Term (operator=PlusOrMinus operand=Term)*;
 ExtraParam: 'param' type=Type (',' attrs=ExtraParamAttr)*  ':' name=ID ('[' size=INT ']')+;
-ExtraParamAttr: 'dup' dup=Number | partitioning=Partitioning;
+ExtraParamAttr: 'dup' dup=Integer | partitioning=Partitioning;
 Factor: (sign=PlusOrMinus)? operand=Operand;
 Float: /(((\d*\.\d+|\d+\.)([+-]?[Ee]\d+)?)|(\d+[+-]?[Ee]\d+))[FfLl]?/;
 Func: name=FuncName '(' operand=Expression (',' operand=Expression)* ')';
@@ -51,7 +51,7 @@ Number: Float|Integer;
 Oct: /0[0-7]+([Uu][Ll][Ll]?|[Ll]?[Ll]?[Uu]?)/;
 Operand: func=Func | name=ID ('[' chan=Integer ']')? '(' idx=INT (',' idx=INT)* ')' | num=Number | '(' expr=Expression ')';
 Output: 'output' type=Type ':' (expr=StageExpr)+;
-Partitioning: 'partition' partition_type='complete' ('dim' '=' dim=Number)? | 'partition' partition_type='cyclic' 'factor' '=' factor=Number ('dim' '=' dim=Number)?;
+Partitioning: 'partition' partition_type='complete' ('dim' '=' dim=Integer)? | 'partition' partition_type='cyclic' 'factor' '=' factor=Integer ('dim' '=' dim=Integer)?;
 PlusOrMinus: '+'|'-';
 StageExpr: name=ID ('[' chan=Integer ']')? '(' idx=INT (',' idx=INT)* ')'
     ('~' depth=Integer)? '=' expr=Expression;
