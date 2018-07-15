a1fcffe3bd1a9e69b04da9cc1d6bfdf946328c48
Blaok <i@blaok.me>
2018-07-14 22:58:12 -0700

    style: use 2-space indent

--- src/tests/soda/grammar.py
+++ src/tests/soda/grammar.py
@@ -7,12 +7,15 @@ from soda import grammar
 
 class TestGrammar(unittest.TestCase):
 
-    def test_syntax(self):
-        soda_mm = textx.metamodel_from_str(
-            grammar.SODA_GRAMMAR,
-            classes=grammar.SODA_GRAMMAR_CLASSES)
-        try:
-            soda_program_str = \
+  def setUp(self):
+    self.ref = grammar.Ref(name='foo', idx=(0, 23), lat=None)
+
+  def test_syntax(self):
+    soda_mm = textx.metamodel_from_str(
+      grammar.SODA_GRAMMAR,
+      classes=grammar.SODA_GRAMMAR_CLASSES)
+    try:
+      soda_program_str = \
 r'''
 border: ignore
 burst width: 512
@@ -42,106 +45,106 @@ output double:
   l = float18_3(c(0, 1) ~5) + a(1, 0)
   e(0, 0) = float15(l + l / 2)
 '''.strip('\n')
-            soda_program = soda_mm.model_from_str(soda_program_str)
-            self.assertEqual(str(soda_program), soda_program_str)
-            return
-        except TextXSyntaxError as e:
-            msg = str(e)
-        self.fail(msg)
+      soda_program = soda_mm.model_from_str(soda_program_str)
+      self.assertEqual(str(soda_program), soda_program_str)
+      return
+    except TextXSyntaxError as e:
+      msg = str(e)
+    self.fail(msg)
 
-    def test_input(self):
-        self.assertEqual(
-            str(grammar.Input(type='int8', name='foo', tile_size=[])),
-            'input int8: foo')
-        self.assertEqual(
-            str(grammar.Input(type='int8', name='foo', tile_size=[23])),
-            'input int8: foo(23,)')
-        self.assertEqual(
-            str(grammar.Input(type='int8', name='foo', tile_size=[23, 233])),
-            'input int8: foo(23, 233,)')
+  def test_input(self):
+    self.assertEqual(
+      str(grammar.InputStmt(soda_type='int8', name='foo', tile_size=[])),
+      'input int8: foo')
+    self.assertEqual(
+      str(grammar.InputStmt(soda_type='int8', name='foo', tile_size=[23])),
+      'input int8: foo(23,)')
+    self.assertEqual(
+      str(grammar.InputStmt(soda_type='int8', name='foo', tile_size=[23, 233])),
+      'input int8: foo(23, 233,)')
 
-    def test_local(self):
-        self.assertEqual(
-            str(grammar.Local(type='int8', let=[],
-                              ref='foo(0, 23)', expr='bar')),
-            'local int8: foo(0, 23) = bar')
-        self.assertEqual(
-            str(grammar.Local(type='int8', let=['let expr'],
-                              ref='foo(0, 23)', expr='bar')),
-            'local int8:\n  let expr\n  foo(0, 23) = bar')
-        self.assertEqual(
-            str(grammar.Local(type='int8', let=['let expr0', 'let expr1'],
-                              ref='foo(0, 23)', expr='bar')),
-            'local int8:\n  let expr0\n  let expr1\n  foo(0, 23) = bar')
+  def test_local(self):
+    self.assertEqual(
+      str(grammar.LocalStmt(soda_type='int8', let=[],
+                ref=self.ref, expr='bar')),
+      'local int8: foo(0, 23) = bar')
+    self.assertEqual(
+      str(grammar.LocalStmt(soda_type='int8', let=['let expr'],
+                ref=self.ref, expr='bar')),
+      'local int8:\n  let expr\n  foo(0, 23) = bar')
+    self.assertEqual(
+      str(grammar.LocalStmt(soda_type='int8', let=['let expr0', 'let expr1'],
+                ref=self.ref, expr='bar')),
+      'local int8:\n  let expr0\n  let expr1\n  foo(0, 23) = bar')
 
-    def test_output(self):
-        self.assertEqual(
-            str(grammar.Output(type='int8', let=[],
-                               ref='foo(0, 23)', expr='bar')),
-            'output int8: foo(0, 23) = bar')
-        self.assertEqual(
-            str(grammar.Output(type='int8', let=['let expr'],
-                               ref='foo(0, 23)', expr='bar')),
-            'output int8:\n  let expr\n  foo(0, 23) = bar')
-        self.assertEqual(
-            str(grammar.Output(type='int8', let=['let expr0', 'let expr1'],
-                               ref='foo(0, 23)', expr='bar')),
-            'output int8:\n  let expr0\n  let expr1\n  foo(0, 23) = bar')
+  def test_output(self):
+    self.assertEqual(
+      str(grammar.OutputStmt(soda_type='int8', let=[],
+                 ref=self.ref, expr='bar')),
+      'output int8: foo(0, 23) = bar')
+    self.assertEqual(
+      str(grammar.OutputStmt(soda_type='int8', let=['let expr'],
+                 ref=self.ref, expr='bar')),
+      'output int8:\n  let expr\n  foo(0, 23) = bar')
+    self.assertEqual(
+      str(grammar.OutputStmt(soda_type='int8', let=['let expr0', 'let expr1'],
+                 ref=self.ref, expr='bar')),
+      'output int8:\n  let expr0\n  let expr1\n  foo(0, 23) = bar')
 
-    def test_let(self):
-        self.assertEqual(str(grammar.Let(type='int8', name='foo', expr='bar')),
-                         'int8 foo = bar')
-        self.assertEqual(str(grammar.Let(type=None, name='foo', expr='bar')),
-                         'foo = bar')
+  def test_let(self):
+    self.assertEqual(str(grammar.Let(soda_type='int8', name='foo', expr='bar')),
+             'int8 foo = bar')
+    self.assertEqual(str(grammar.Let(soda_type=None, name='foo', expr='bar')),
+             'foo = bar')
 
-    def test_ref(self):
-        self.assertEqual(str(grammar.Ref(name='foo', idx=[0], lat=None)),
-                         'foo(0)')
-        self.assertEqual(str(grammar.Ref(name='foo', idx=[0], lat=233)),
-                         'foo(0) ~233')
-        self.assertEqual(str(grammar.Ref(name='foo', idx=[0, 23], lat=233)),
-                         'foo(0, 23) ~233')
+  def test_ref(self):
+    self.assertEqual(str(grammar.Ref(name='foo', idx=[0], lat=None)),
+             'foo(0)')
+    self.assertEqual(str(grammar.Ref(name='foo', idx=[0], lat=233)),
+             'foo(0) ~233')
+    self.assertEqual(str(grammar.Ref(name='foo', idx=[0, 23], lat=233)),
+             'foo(0, 23) ~233')
 
-    def test_binary_operations(self):
-        for operand, operators in [
-                (grammar.Expr, ('||',)),
-                (grammar.LogicAnd, ('&&',)),
-                (grammar.BinaryOr, ('|',)),
-                (grammar.Xor, ('^',)),
-                (grammar.BinaryAnd, ('&',)),
-                (grammar.EqCmp, ('==', '!=')),
-                (grammar.LtCmp, ('<=', '>=', '<', '>')),
-                (grammar.AddSub, ('+', '-')),
-                (grammar.MulDiv, ('*', '/', '%'))]:
-            self.assertEqual(
-                str(operand(operand=list(map('op{}'.format,
-                                             range(len(operators)+1))),
-                            operator=operators)),
-                'op0'+''.join(' {} op{}'.format(op, idx+1)
-                              for idx, op in enumerate(operators)))
+  def test_binary_operations(self):
+    for operand, operators in [
+        (grammar.Expr, ('||',)),
+        (grammar.LogicAnd, ('&&',)),
+        (grammar.BinaryOr, ('|',)),
+        (grammar.Xor, ('^',)),
+        (grammar.BinaryAnd, ('&',)),
+        (grammar.EqCmp, ('==', '!=')),
+        (grammar.LtCmp, ('<=', '>=', '<', '>')),
+        (grammar.AddSub, ('+', '-')),
+        (grammar.MulDiv, ('*', '/', '%'))]:
+      self.assertEqual(
+        str(operand(operand=list(map('op{}'.format,
+                       range(len(operators)+1))),
+              operator=operators)),
+        'op0'+''.join(' {} op{}'.format(op, idx+1)
+                for idx, op in enumerate(operators)))
 
-    def test_unary(self):
-        self.assertEqual(str(grammar.Unary(operator='+-~!'.split(),
-                                           operand='op')),
-                         '+-~!op')
+  def test_unary(self):
+    self.assertEqual(str(grammar.Unary(operator='+-~!'.split(),
+                       operand='op')),
+             '+-~!op')
 
-    def test_cast(self):
-        self.assertEqual(str(grammar.Cast(type='int8', expr='expr')),
-                         'int8(expr)')
+  def test_cast(self):
+    self.assertEqual(str(grammar.Cast(soda_type='int8', expr='expr')),
+             'int8(expr)')
 
-    def test_call(self):
-        self.assertEqual(str(grammar.Call(name='pi', arg=[])),
-                         'pi()')
-        self.assertEqual(str(grammar.Call(name='sqrt', arg=['arg'])),
-                         'sqrt(arg)')
-        self.assertEqual(str(grammar.Call(
-            name='select', arg=['condition', 'true_val', 'false_val'])),
-                         'select(condition, true_val, false_val)')
+  def test_call(self):
+    self.assertEqual(str(grammar.Call(name='pi', arg=[])),
+             'pi()')
+    self.assertEqual(str(grammar.Call(name='sqrt', arg=['arg'])),
+             'sqrt(arg)')
+    self.assertEqual(str(grammar.Call(
+      name='select', arg=['condition', 'true_val', 'false_val'])),
+             'select(condition, true_val, false_val)')
 
-    def test_var(self):
-        self.assertEqual(str(grammar.Var(name='foo', idx=[])), 'foo')
-        self.assertEqual(str(grammar.Var(name='foo', idx=[0])), 'foo[0]')
-        self.assertEqual(str(grammar.Var(name='foo', idx=[0, 1])), 'foo[0][1]')
+  def test_var(self):
+    self.assertEqual(str(grammar.Var(name='foo', idx=[])), 'foo')
+    self.assertEqual(str(grammar.Var(name='foo', idx=[0])), 'foo[0]')
+    self.assertEqual(str(grammar.Var(name='foo', idx=[0, 1])), 'foo[0][1]')
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
