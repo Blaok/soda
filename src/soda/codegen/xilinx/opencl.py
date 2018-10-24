af3eb26e3e1424ea0467b54d0be8ab925b29ffba
Blaok <i@blaok.me>
2018-10-24 12:17:13 -0700

    feat: do not open the output file if failed

--- src/soda/codegen/xilinx/opencl.py
+++ src/soda/codegen/xilinx/opencl.py
@@ -1,5 +1,7 @@
-import sys
 import os
+import shutil
+import sys
+import tempfile
 
 from soda.codegen.xilinx import header
 from soda.codegen.xilinx import host
@@ -24,25 +26,34 @@ def add_arguments(parser):
 
 def print_code(stencil, args):
   if args.kernel_file is not None:
-    if args.kernel_file == '-':
-      kernel.print_code(stencil, sys.stdout)
-    else:
-      with open(args.kernel_file, 'w') as kernel_file:
-        kernel.print_code(stencil, kernel_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      kernel.print_code(stencil, tmp)
+      tmp.seek(0)
+      if args.kernel_file == '-':
+        shutil.copyfileobj(tmp, sys.stdout)
+      else:
+        with open(args.kernel_file, 'w') as kernel_file:
+          shutil.copyfileobj(tmp, kernel_file)
 
   if args.host_file is not None:
-    if args.host_file == '-':
-      host.print_code(stencil, sys.stdout)
-    else:
-      with open(args.host_file, 'w') as host_file:
-        host.print_code(stencil, host_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      host.print_code(stencil, tmp)
+      tmp.seek(0)
+      if args.host_file == '-':
+        shutil.copyfileobj(tmp, sys.stdout)
+      else:
+        with open(args.host_file, 'w') as host_file:
+          shutil.copyfileobj(tmp, host_file)
 
   if args.header_file is not None:
-    if args.header_file == '-':
-      header.print_code(stencil, sys.stdout)
-    else:
-      with open(args.header_file, 'w') as header_file:
-        header.print_code(stencil, header_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      header.print_code(stencil, tmp)
+      tmp.seek(0)
+      if args.header_file == '-':
+        shutil.copyfileobj(tmp, sys.stdout)
+      else:
+        with open(args.header_file, 'w') as header_file:
+          shutil.copyfileobj(tmp, header_file)
 
   if args.output_dir is not None and (args.kernel_file is None or
                                       args.host_file is None or
@@ -57,17 +68,26 @@ def print_code(stencil, args):
               stencil.unroll_factor, dram_in + '-' + dram_out))
     else:
       kernel_file_name = args.kernel_file
-    with open(kernel_file_name, 'w') as kernel_file:
-      kernel.print_code(stencil, kernel_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      kernel.print_code(stencil, tmp)
+      tmp.seek(0)
+      with open(kernel_file_name, 'w') as kernel_file:
+        shutil.copyfileobj(tmp, kernel_file)
     if args.host_file is None:
       host_file_name = os.path.join(args.output_dir, stencil.app_name + '.cpp')
     else:
       host_file_name = args.host_file
-    with open(host_file_name, 'w') as host_file:
-      host.print_code(stencil, host_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      host.print_code(stencil, tmp)
+      tmp.seek(0)
+      with open(host_file_name, 'w') as host_file:
+        shutil.copyfileobj(tmp, kernel_file)
     if args.header_file is None:
       header_file_name = os.path.join(args.output_dir, stencil.app_name + '.h')
     else:
       header_file_name = args.header_file
-    with open(header_file_name, 'w') as header_file:
-      header.print_code(stencil, header_file)
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+      header.print_code(stencil, tmp)
+      tmp.seek(0)
+      with open(header_file_name, 'w') as header_file:
+        shutil.copyfileobj(tmp, header_file)
