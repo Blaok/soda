c8f3f68564ceb2cf19aef88388d2a403d60b75b7
Blaok <i@blaok.me>
2018-11-19 11:39:06 -0800

    chore: return correct value for pre-commit target

--- src/pylint.sh
+++ src/pylint.sh
@@ -4,7 +4,9 @@ then
   jobslots=$(timeout 0.01 cat <&${BASH_REMATCH[1]})
   echo pylint -j${#jobslots} "$@"
   pylint -j${#jobslots} "$@"
+  rc=$?
   echo -n "${jobslots}" >&${BASH_REMATCH[2]}
+  exit $rc
 else
   echo pylint "$@"
   exec pylint "$@"
