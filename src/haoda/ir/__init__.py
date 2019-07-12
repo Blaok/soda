5fc3219ef7f354e6fce0297c093a700ca41cf8d2
Blaok <i@blaok.me>
2019-07-12 12:54:40 -0700

    style(haoda): apply formater

--- src/haoda/ir/__init__.py
+++ src/haoda/ir/__init__.py
@@ -1,6 +1,5 @@
 from typing import (
-    Tuple,
-)
+    Tuple,)
 
 import collections
 import copy
@@ -76,11 +75,12 @@ Var: name=ID ('[' idx=Int ']')*;
 
 '''
 
+
 class Node():
   """A immutable, hashable IR node.
   """
-  SCALAR_ATTRS = ()   # type: Tuple[str, ...]
-  LINEAR_ATTRS = ()   # type: Tuple[str, ...]
+  SCALAR_ATTRS = ()  # type: Tuple[str, ...]
+  LINEAR_ATTRS = ()  # type: Tuple[str, ...]
 
   @property
   def ATTRS(self):
@@ -97,9 +97,9 @@ class Node():
                  tuple(tuple(getattr(self, _)) for _ in self.LINEAR_ATTRS)))
 
   def __eq__(self, other):
-    return all(hasattr(other, attr) and
-               getattr(self, attr) == getattr(other, attr)
-               for attr in self.ATTRS)
+    return all(
+        hasattr(other, attr) and getattr(self, attr) == getattr(other, attr)
+        for attr in self.ATTRS)
 
   @property
   def c_type(self):
@@ -139,16 +139,19 @@ class Node():
     if obj is not self_copy:
       return obj
     self_copy = callback_wrapper(pre_recursion, copy.copy(self), args)
-    scalar_attrs = {attr: getattr(self_copy, attr).visit(
-        callback, args, pre_recursion, post_recursion)
-                    if isinstance(getattr(self_copy, attr), Node)
-                    else getattr(self_copy, attr)
-                    for attr in self_copy.SCALAR_ATTRS}
-    linear_attrs = {attr: tuple(_.visit(
-        callback, args, pre_recursion, post_recursion)
-                                if isinstance(_, Node) else _
-                                for _ in getattr(self_copy, attr))
-                    for attr in self_copy.LINEAR_ATTRS}
+    scalar_attrs = {
+        attr: getattr(self_copy, attr).visit(
+            callback, args, pre_recursion, post_recursion) if isinstance(
+                getattr(self_copy, attr), Node) else getattr(self_copy, attr)
+        for attr in self_copy.SCALAR_ATTRS
+    }
+    linear_attrs = {
+        attr: tuple(
+            _.visit(callback, args, pre_recursion, post_recursion
+                   ) if isinstance(_, Node) else _
+            for _ in getattr(self_copy, attr))
+        for attr in self_copy.LINEAR_ATTRS
+    }
 
     for attr in self.SCALAR_ATTRS:
       # old attribute may not exist in mutated object
@@ -161,11 +164,12 @@ class Node():
       # old attribute may not exist in mutated object
       if not hasattr(obj, attr):
         continue
-      setattr(obj, attr, tuple(
-          c if a is b and isinstance(a, Node) else a
-          for a, b, c in zip(getattr(obj, attr), getattr(self, attr),
-                             linear_attrs[attr])))
+      setattr(
+          obj, attr,
+          tuple(c if a is b and isinstance(a, Node) else a for a, b, c in zip(
+              getattr(obj, attr), getattr(self, attr), linear_attrs[attr])))
     return callback_wrapper(post_recursion, obj, args)
+
 
 class Let(Node):
   SCALAR_ATTRS = 'haoda_type', 'name', 'expr'
@@ -191,9 +195,11 @@ class Let(Node):
     return 'const {} {} = {};'.format(self.c_type, self.name,
                                       unparenthesize(self.expr.c_expr))
 
+
 class Ref(Node):
   SCALAR_ATTRS = 'name', 'lat'
   LINEAR_ATTRS = ('idx',)
+
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
     self.idx = tuple(self.idx)
@@ -210,8 +216,10 @@ class Ref(Node):
       result += ' ~{}'.format(self.lat)
     return result
 
+
 class BinaryOp(Node):
   LINEAR_ATTRS = 'operand', 'operator'
+
   def __str__(self):
     result = str(self.operand[0])
     for operator, operand in zip(self.operator, self.operand[1:]):
@@ -222,7 +230,7 @@ class BinaryOp(Node):
 
   @property
   def haoda_type(self):
-  # TODO: derive from all operands
+    # TODO: derive from all operands
     return self.operand[0].haoda_type
 
   @property
@@ -236,40 +244,51 @@ class BinaryOp(Node):
 
   @property
   def singleton(self) -> bool:
-    return len(self.operand) == 1   # type: ignore
+    return len(self.operand) == 1  # type: ignore
+
 
 class Expr(BinaryOp):
   pass
 
+
 class LogicAnd(BinaryOp):
   pass
+
 
 class BinaryOr(BinaryOp):
   pass
 
+
 class Xor(BinaryOp):
   pass
+
 
 class BinaryAnd(BinaryOp):
   pass
 
+
 class EqCmp(BinaryOp):
   pass
+
 
 class LtCmp(BinaryOp):
   pass
 
+
 class AddSub(BinaryOp):
   pass
+
 
 class MulDiv(BinaryOp):
   pass
 
+
 class Unary(Node):
   SCALAR_ATTRS = ('operand',)
   LINEAR_ATTRS = ('operator',)
+
   def __str__(self):
-    return ''.join(self.operator)+str(self.operand)
+    return ''.join(self.operator) + str(self.operand)
 
   @property
   def haoda_type(self):
@@ -277,10 +296,12 @@ class Unary(Node):
 
   @property
   def c_expr(self):
-    return ''.join(self.operator)+self.operand.c_expr
+    return ''.join(self.operator) + self.operand.c_expr
+
 
 class Operand(Node):
   SCALAR_ATTRS = 'cast', 'call', 'ref', 'num', 'var', 'expr'
+
   def __str__(self):
     for attr in ('cast', 'call', 'ref', 'num', 'var'):
       if getattr(self, attr) is not None:
@@ -325,8 +346,10 @@ class Operand(Node):
         return None
     raise util.InternalError('undefined Operand')
 
+
 class Cast(Node):
   SCALAR_ATTRS = 'haoda_type', 'expr'
+
   def __str__(self):
     return '{}{}'.format(self.haoda_type, parenthesize(self.expr))
 
@@ -335,9 +358,11 @@ class Cast(Node):
     return 'static_cast<{} >{}'.format(self.c_type,
                                        parenthesize(self.expr.c_expr))
 
+
 class Call(Node):
   SCALAR_ATTRS = ('name',)
   LINEAR_ATTRS = ('arg',)
+
   def __str__(self):
     return '{}({})'.format(self.name, ', '.join(map(str, self.arg)))
 
@@ -351,15 +376,18 @@ class Call(Node):
   def c_expr(self):
     return '{}({})'.format(self.name, ', '.join(_.c_expr for _ in self.arg))
 
+
 class Var(Node):
   SCALAR_ATTRS = ('name',)
   LINEAR_ATTRS = ('idx',)
+
   def __str__(self):
-    return self.name+''.join(map('[{}]'.format, self.idx))
+    return self.name + ''.join(map('[{}]'.format, self.idx))
 
   @property
   def c_expr(self):
-    return self.name+''.join(map('[{}]'.format, self.idx))
+    return self.name + ''.join(map('[{}]'.format, self.idx))
+
 
 class FIFO(Node):
   """A reference to another node in a haoda.ir.Expr.
@@ -378,23 +406,32 @@ class FIFO(Node):
   IMMUTABLE_ATTRS = 'read_module', 'write_module'
   SCALAR_ATTRS = 'read_module', 'read_lat', 'write_module', 'write_lat', 'depth'
 
-  def __init__(self, write_module, read_module,
-               depth=None, write_lat=None, read_lat=None):
-    super().__init__(write_module=write_module, read_module=read_module,
-                     depth=depth, write_lat=write_lat, read_lat=read_lat)
+  def __init__(self,
+               write_module,
+               read_module,
+               depth=None,
+               write_lat=None,
+               read_lat=None):
+    super().__init__(write_module=write_module,
+                     read_module=read_module,
+                     depth=depth,
+                     write_lat=write_lat,
+                     read_lat=read_lat)
 
   def __repr__(self):
-    return 'fifo[%d]: %s%s => %s%s' % (self.depth, repr(self.write_module),
-      '' if self.write_lat is None else ' ~%s'%self.write_lat,
-      repr(self.read_module),
-      '' if self.read_lat is None else ' ~%s'%self.read_lat)
+    return 'fifo[%d]: %s%s => %s%s' % (
+        self.depth, repr(self.write_module), '' if self.write_lat is None else
+        ' ~%s' % self.write_lat, repr(self.read_module),
+        '' if self.read_lat is None else ' ~%s' % self.read_lat)
 
   def __hash__(self):
     return hash(tuple(getattr(self, _) for _ in self.IMMUTABLE_ATTRS))
 
   def __eq__(self, other):
-    return all(getattr(self, _) == getattr(other, _)
-               for _ in type(self).IMMUTABLE_ATTRS)
+    return all(
+        getattr(self, _) == getattr(other, _)
+        for _ in type(self).IMMUTABLE_ATTRS)
+
   @property
   def edge(self):
     return self.write_module, self.read_module
@@ -406,6 +443,7 @@ class FIFO(Node):
   @property
   def c_expr(self):
     return 'from_{}_to_{}'.format(self.write_module.name, self.read_module.name)
+
 
 class Module():
   """A node in the dataflow graph.
@@ -420,6 +458,7 @@ class Module():
     lets: List of haoda.ir.Let expressions.
     exprs: Dict of {FIFO: haoda.ir.Expr}, stores an output's expression.
   """
+
   def __init__(self):
     """Initializes attributes into empty list or dict.
     """
@@ -482,8 +521,8 @@ class Module():
     dram_reads = tuple(dram_reads.values())
 
     # find dram writes
-    writes_in_lets = tuple(_.name for _ in self.lets
-                           if not isinstance(_.name, str))
+    writes_in_lets = tuple(
+        _.name for _ in self.lets if not isinstance(_.name, str))
     dram_writes = collections.OrderedDict()
     for dram_ref in visitor.get_dram_refs(writes_in_lets):
       for bank in dram_ref.dram:
@@ -493,17 +532,15 @@ class Module():
     output_fifos = tuple(_.c_expr for _ in self.exprs)
     input_fifos = tuple(_.c_expr for _ in visitor.get_read_fifo_set(self))
 
-
     return {
-        'dram_writes' : dram_writes,
-        'output_fifos' : output_fifos,
-        'input_fifos' : input_fifos,
-        'dram_reads' : dram_reads
+        'dram_writes': dram_writes,
+        'output_fifos': output_fifos,
+        'input_fifos': input_fifos,
+        'dram_reads': dram_reads
     }
 
   def __str__(self):
-    return '%s @ 0x%x: %s' % (type(self).__name__, id(self),
-      self.__dict__)
+    return '%s @ 0x%x: %s' % (type(self).__name__, id(self), self.__dict__)
 
   def __repr__(self):
     return '%s @ 0x%x' % (type(self).__name__, id(self))
@@ -620,8 +657,8 @@ class Module():
     Returns:
       Set of descendant (src Module, dst Module) tuple.
     """
-    return ({(self, child) for child in self.children}
-        .union(*map(Module.get_connections, self.children)))
+    return ({(self, child) for child in self.children
+            }.union(*map(Module.get_connections, self.children)))
 
 
 class DelayedRef(Node):
@@ -632,6 +669,7 @@ class DelayedRef(Node):
     ref: FIFO
   """
   SCALAR_ATTRS = ('delay', 'ref')
+
   @property
   def haoda_type(self):
     return self.ref.haoda_type
@@ -646,8 +684,9 @@ class DelayedRef(Node):
     return hash((self.delay, self.ref))
 
   def __eq__(self, other):
-    return all(getattr(self, attr) == getattr(other, attr)
-               for attr in ('delay', 'ref'))
+    return all(
+        getattr(self, attr) == getattr(other, attr)
+        for attr in ('delay', 'ref'))
 
   @property
   def buf_name(self):
@@ -659,7 +698,7 @@ class DelayedRef(Node):
 
   @property
   def ptr_type(self):
-    return 'uint%d' % int(math.log2(self.delay)+1)
+    return 'uint%d' % int(math.log2(self.delay) + 1)
 
   @property
   def c_expr(self):
@@ -692,7 +731,8 @@ class DelayedRef(Node):
   @property
   def c_next_ptr_expr(self):
     return '{ptr} < {depth} ? {c_ptr_type}({ptr}+1) : {c_ptr_type}(0)'.format(
-        ptr=self.ptr, c_ptr_type=self.c_ptr_type, depth=self.delay-1)
+        ptr=self.ptr, c_ptr_type=self.c_ptr_type, depth=self.delay - 1)
+
 
 class FIFORef(Node):
   """A FIFO reference.
@@ -713,9 +753,10 @@ class FIFORef(Node):
   LD_PREFIX = 'fifo_ld_'
   ST_PREFIX = 'fifo_st_'
   REF_PREFIX = 'fifo_ref_'
+
   def __str__(self):
     return '<%s fifo_ref_%d%s>' % (self.haoda_type, self.ref_id,
-                                   '@%s'%self.lat if self.lat else '')
+                                   '@%s' % self.lat if self.lat else '')
 
   def __repr__(self):
     return str(self)
@@ -724,8 +765,9 @@ class FIFORef(Node):
     return hash((self.lat, self.ref_id))
 
   def __eq__(self, other):
-    return all(getattr(self, attr) == getattr(other, attr)
-               for attr in ('lat', 'ref_id'))
+    return all(
+        getattr(self, attr) == getattr(other, attr)
+        for attr in ('lat', 'ref_id'))
 
   @property
   def haoda_type(self):
@@ -743,6 +785,7 @@ class FIFORef(Node):
   def c_expr(self):
     return self.ref_name
 
+
 class DRAMRef(Node):
   """A DRAM reference.
 
@@ -753,9 +796,10 @@ class DRAMRef(Node):
     offset: int
   """
   SCALAR_ATTRS = 'haoda_type', 'dram', 'var', 'offset'
+
   def __str__(self):
-    return 'dram<bank {} {}@{}>'.format(util.lst2str(self.dram),
-                                        self.var, self.offset)
+    return 'dram<bank {} {}@{}>'.format(util.lst2str(self.dram), self.var,
+                                        self.offset)
 
   def __repr__(self):
     return str(self)
@@ -764,8 +808,10 @@ class DRAMRef(Node):
     return hash((self.dram, self.offset))
 
   def __eq__(self, other):
-    return all(getattr(self, attr) == getattr(other, attr)
-               for attr in ('dram', 'offset'))
+    return all(
+        getattr(self, attr) == getattr(other, attr)
+        for attr in ('dram', 'offset'))
+
   @property
   def c_expr(self):
     return str(self)
@@ -777,6 +823,7 @@ class DRAMRef(Node):
   def dram_fifo_name(self, bank):
     assert bank in self.dram, 'unexpected bank {}'.format(bank)
     return 'dram_{}_bank_{}_fifo'.format(self.var, bank)
+
 
 class ModuleTrait(Node):
   """A immutable, hashable trait of a dataflow module.
@@ -793,11 +840,12 @@ class ModuleTrait(Node):
   LINEAR_ATTRS = ('lets', 'exprs', 'template_types', 'template_ints')
 
   def __init__(self, node):
+
     def mutate(obj, loads):
       if isinstance(obj, FIFO):
         if loads:
           if obj not in loads:
-            load_id = next(reversed(loads.values())).ref_id+1
+            load_id = next(reversed(loads.values())).ref_id + 1
           else:
             return loads[obj]
         else:
@@ -806,19 +854,20 @@ class ModuleTrait(Node):
         loads[obj] = fifo_ref
         return fifo_ref
       return obj
+
     loads = collections.OrderedDict()
     node = node.visit_loads(mutate, loads)
     self.loads = tuple(loads.values())
-    super().__init__(lets=tuple(node.lets), exprs=tuple(node.exprs.values()),
-                     template_types=tuple(), template_ints=tuple())
+    super().__init__(lets=tuple(node.lets),
+                     exprs=tuple(node.exprs.values()),
+                     template_types=tuple(),
+                     template_ints=tuple())
     _logger.debug('Signature: %s', self)
 
   def __repr__(self):
     return '%s(loads: %s, lets: %s, exprs: %s)' % (
-        type(self).__name__,
-        util.idx2str(self.loads),
-        util.idx2str(self.lets),
-        util.idx2str(self.exprs))
+        type(self).__name__, util.idx2str(self.loads), util.idx2str(
+            self.lets), util.idx2str(self.exprs))
 
   @property
   def dram_reads(self):
@@ -848,8 +897,8 @@ class ModuleTrait(Node):
     dram_reads = tuple(dram_reads.values())
 
     # find dram writes
-    writes_in_lets = tuple(_.name for _ in self.lets
-                           if not isinstance(_.name, str))
+    writes_in_lets = tuple(
+        _.name for _ in self.lets if not isinstance(_.name, str))
     dram_writes = collections.OrderedDict()
     for dram_ref in visitor.get_dram_refs(writes_in_lets):
       for bank in dram_ref.dram:
@@ -861,15 +910,17 @@ class ModuleTrait(Node):
     input_fifos = tuple(_.ld_name for _ in self.loads)
 
     return {
-        'dram_writes' : dram_writes,
-        'output_fifos' : output_fifos,
-        'input_fifos' : input_fifos,
-        'dram_reads' : dram_reads
+        'dram_writes': dram_writes,
+        'output_fifos': output_fifos,
+        'input_fifos': input_fifos,
+        'dram_reads': dram_reads
     }
+
 
 def make_var(val):
   """Make literal Var from val."""
   return Var(name=val, idx=())
+
 
 def str2int(s, none_val=None):
   if s is None:
@@ -884,8 +935,10 @@ def str2int(s, none_val=None):
     return int(s, 8)
   return int(s)
 
+
 def parenthesize(expr) -> str:
   return '({})'.format(unparenthesize(expr))
+
 
 def unparenthesize(expr) -> str:
   expr_str = str(expr)
@@ -901,10 +954,11 @@ def unparenthesize(expr) -> str:
     expr_str = expr_str[1:-1]
   return expr_str
 
+
 def get_result_type(operand1, operand2, operator):
-  for t in ('double', 'float') + sum((('int%d_t'%w, 'uint%d_t'%w)
-                                      for w in (64, 32, 16, 8)), tuple()):
+  for t in ('double', 'float') + sum(
+      (('int%d_t' % w, 'uint%d_t' % w) for w in (64, 32, 16, 8)), tuple()):
     if t in (operand1, operand2):
       return t
   raise util.SemanticError('cannot parse type: %s %s %s' %
-    (operand1, operator, operand2))
+                           (operand1, operator, operand2))
