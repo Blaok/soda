330143badc48459e75385be693eff1254162f72c
Blaok <i@blaok.me>
2019-07-12 12:35:08 -0700

    fix(grammar): move FuncName to haoda grammar

--- src/haoda/ir/__init__.py
+++ src/haoda/ir/__init__.py
@@ -60,6 +60,15 @@ MulDivOp: '*'|'/'|'%';
 Unary: (operator=UnaryOp)* operand=Operand;
 UnaryOp: '+'|'-'|'~'|'!';
 
+FuncName: 'cos'|'sin'|'tan'|'acos'|'asin'|'atan'|'atan2'|
+  'cosh'|'sinh'|'tanh'|'acosh'|'asinh'|'atanh'|
+  'exp'|'frexp'|'ldexp'|'log'|'log10'|'modf'|'exp2'|'expm1'|'ilogb'|'log1p'|'log2'|'logb'|'scalbn'|'scalbln'|
+  'pow'|'sqrt'|'cbrt'|'hypot'|
+  'erf'|'erfc'|'tgamma'|'lgamma'|
+  'ceil'|'floor'|'fmod'|'trunc'|'round'|'lround'|'llround'|'rint'|'lrint'|'llrint'|'nearbyint'|'remainder'|'remquo'|
+  'copysign'|'nan'|'nextafter'|'nexttoward'|'fdim'|'fmax'|'fmin'|'fabs'|'abs'|'fma'|
+  'min'|'max'|'select';
+
 Operand: cast=Cast | call=Call | ref=Ref | num=Num | var=Var | '(' expr=Expr ')';
 Cast: haoda_type=Type '(' expr=Expr ')';
 Call: name=FuncName '(' arg=Expr (',' arg=Expr)* ')';
