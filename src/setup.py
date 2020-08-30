04b323dc6fb5911a2ee597e9c3eacf41d9cc2959
Blaok <i@blaok.me>
2020-08-29 18:09:24 -0700

    chore: bump version

--- src/setup.py
+++ src/setup.py
@@ -15,7 +15,7 @@ with open('../README.md', encoding='utf-8') as f:
 
 setup(
     name='sodac',
-    version='0.0.20200601.dev1',
+    version='0.0.20200829.dev1',
     description='Stencil with optimized dataflow architecture',
     long_description=long_description,
     long_description_content_type='text/markdown',
